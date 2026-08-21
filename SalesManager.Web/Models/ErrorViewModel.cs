886768f039dc18d450986c4fdc4d37e8eb24bb91
iclasTH <iclasinprogramacao@gmail.com>
2026-08-21 12:32:38 -0300

    feat: add Web application initial files

---
+++ SalesManager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SalesManager.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
