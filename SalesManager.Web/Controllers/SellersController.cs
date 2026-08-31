0b1b8fe3227543acc1c8427a5667b028d7741b13
iclasTH <iclasinprogramacao@gmail.com>
2026-08-31 12:06:25 -0300

    feat: implement seller's create end-point
    call a view, built as a form, to create sellers. isn't functional yet cause need to implement the non-null field department

--- SalesManager.Web/Controllers/SellersController.cs
+++ SalesManager.Web/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using SalesManager.Web.Models;
 using SalesManager.Web.Services;
 
 namespace SalesManager.Web.Controllers;
@@ -17,5 +18,20 @@ public class SellersController : Controller
     {
         var sellers = _sellerService.FindAll();
         return View(sellers);
+    }
+    
+    // Get
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    // Post
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Seller seller)
+    {
+        _sellerService.Insert(seller);
+        return RedirectToAction(nameof(Index));
     }
 }
--- SalesManager.Web/Services/SellerService.cs
+++ SalesManager.Web/Services/SellerService.cs
@@ -16,4 +16,10 @@ public class SellerService
     {
         return _context.Seller.ToList();
     }
+
+    public void Insert(Seller seller)
+    {
+        _context.Add(seller);
+        _context.SaveChanges();
+    }
 }
