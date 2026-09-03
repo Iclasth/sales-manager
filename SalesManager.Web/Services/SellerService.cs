3a4265404424f65e7a943b88734a90cbd5d96782
iclasTH <iclasinprogramacao@gmail.com>
2026-09-03 17:52:52 -0300

    feat: add delete and findById services
    first of all, create a service to find a seller by its id. using this service, implement entity remove logic and save changes in database. in the end, create two end-points: a get end point to call the delete view and a post to apply changes. in the view, it was created a confirm screen to  assure the action

--- SalesManager.Web/Services/SellerService.cs
+++ SalesManager.Web/Services/SellerService.cs
@@ -22,4 +22,17 @@ public class SellerService
         _context.Add(seller);
         _context.SaveChanges();
     }
+
+    public Seller FindById(int id)
+    {
+        return _context.Seller.FirstOrDefault(sl => sl.Id == id);
+    }
+
+    public void Remove(int id)
+    {
+        var seller = FindById(id);
+        _context.Seller.Remove(seller);
+        _context.SaveChanges();
+    }
+    
 }
